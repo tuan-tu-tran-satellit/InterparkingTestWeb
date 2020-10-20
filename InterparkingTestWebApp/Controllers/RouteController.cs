aa3d40ba8b0cb7788146e41bfde4e20c84ab7d14
Tuan-Tu Tran <tuan-tu.tran@satellit.be>
2020-10-20 12:56:28 +0200

    add a basic Create form

--- InterparkingTestWebApp/Controllers/RouteController.cs
+++ InterparkingTestWebApp/Controllers/RouteController.cs
@@ -4,14 +4,45 @@ using System.Linq;
 using System.Threading.Tasks;
 using InterparkingTestWebApp.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace InterparkingTestWebApp.Controllers
 {
     public class RouteController : Controller
     {
+        private readonly ILogger<RouteController> _logger;
+
+        public RouteController(ILogger<RouteController> logger)
+        {
+            _logger = logger;
+        }
+
         public IActionResult Index()
         {
             return View(new List<Route>());
+        }
+
+        public IActionResult Create()
+        {
+            var defaultRoute = new Route()
+            {
+                StartLatitude = 123,
+                StartLongitude = 123,
+                StopLatitude = 123,
+                StopLongitude = 123,
+            };
+            return View(defaultRoute);
+        }
+
+        [HttpPost]
+        public IActionResult Create(Route route)
+        {
+            if (ModelState.IsValid)
+            {
+                _logger.LogDebug("save route: {0}");
+                return RedirectToAction(nameof(Index));
+            }
+            return View();
         }
     }
 }
