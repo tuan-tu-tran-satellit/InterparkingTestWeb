e1bf6df55e3170325afbf38958aae53e874a91cc
Tuan-Tu Tran <tuan-tu.tran@satellit.be>
2020-10-20 18:06:20 +0200

    add test for routing service

---
+++ InterparkingTest.Application.Tests/Services/RoutingServiceTest.cs
@@ -0,0 +1,34 @@
+﻿using FluentAssertions;
+using Geolocation;
+using InterparkingTest.Application.Domain;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Runtime.InteropServices.ComTypes;
+using System.Threading.Tasks;
+
+namespace InterparkingTest.Application.Services
+{
+    [TestClass]
+    public class RoutingServiceTest
+    {
+        [TestMethod]
+        public async Task CalculateDistance()
+        {
+            var routingService = new RoutingService();
+            var a = new Coordinates()
+            {
+                Latitude = 50.125468,
+                Longitude = 4.136589,
+            };
+
+            var b = new Coordinates()
+            {
+                Latitude = 54.126857,
+                Longitude = 4.879536,
+            };
+
+            var distance = await routingService.CalculateDistanceAsync(a, b, new System.Threading.CancellationToken());
+            distance.Should().BeApproximately(444, 1);
+        }
+
+    }
+}
