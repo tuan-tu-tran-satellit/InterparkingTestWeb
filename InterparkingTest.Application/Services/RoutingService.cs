d81419070c99b714da52f7b411525111cd3976e7
Tuan-Tu Tran <tuan-tu.tran@satellit.be>
2020-10-20 16:06:22 +0200

    add a routing service

---
+++ InterparkingTest.Application/Services/RoutingService.cs
@@ -0,0 +1,31 @@
+﻿using InterparkingTest.Application.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterparkingTest.Application.Services
+{
+    class RoutingService : IRoutingService
+    {
+        public Task<double> CalculateDistanceAsync(Coordinates startPoint, Coordinates endPoint)
+        {
+            //Simple implementation that calculates the "crow-flight" distance
+            var a = new Geolocation.Coordinate()
+            {
+                Latitude = startPoint.Latitude,
+                Longitude = endPoint.Longitude,
+            };
+
+            var b = new Geolocation.Coordinate()
+            {
+                Latitude = endPoint.Latitude,
+                Longitude = endPoint.Longitude,
+            };
+
+            var distance = Geolocation.GeoCalculator.GetDistance(a, b, decimalPlaces: 3, distanceUnit: Geolocation.DistanceUnit.Kilometers);
+            
+            return Task.FromResult(distance);
+        }
+    }
+}
