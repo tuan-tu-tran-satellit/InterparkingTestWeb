86ff1a5163ae0dd237ab1c0b46997ca9fbb30f14
Tuan-Tu Tran <tuan-tu.tran@satellit.be>
2020-10-20 21:16:54 +0200

    add extension method to register services

---
+++ InterparkingTest.Application.Tests/ServiceCollectionExtentionTest.cs
@@ -0,0 +1,30 @@
+﻿using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace InterparkingTest.Application
+{
+    [TestClass]
+    public class ServiceCollectionExtentionTest
+    {
+        [TestMethod]
+        public void ResolveApplicationFacade()
+        {
+            //Arrange
+            var services = new ServiceCollection();
+            services.AddInterparkingTestApplicationServices();
+            var provider = services.BuildServiceProvider();
+
+            using (var scope = provider.CreateScope())
+            {
+                //Act
+                Action resolve = () =>
+                    scope.ServiceProvider.GetRequiredService<IApplicationFacade>();
+
+                //Assert
+                resolve.Should().NotThrow();
+            }
+        }
+    }
+}
---
+++ InterparkingTest.Application/ServiceCollectionExtension.cs
@@ -0,0 +1,25 @@
+﻿using InterparkingTest.Application.Commands.CreateRoute;
+using InterparkingTest.Application.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InterparkingTest.Application
+{
+    public static class ServiceCollectionExtension
+    {
+        public static IServiceCollection AddInterparkingTestApplicationServices(this IServiceCollection services, Action<DbContextOptionsBuilder> optionsAction = null)
+        {
+            services
+                .AddScoped<IApplicationFacade, ApplicationFacade>()
+                .AddDbContext<IRouteRepository, RouteRepository>(optionsAction)
+                .AddScoped<IRouteModificationService, RouteModificationService>()
+                .AddScoped<IRoutingService, RoutingService>()
+                .AddScoped<IFuelConsumptionService, FuelConsumptionService>()
+            ;
+            return services;
+        }
+    }
+}
