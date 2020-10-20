784b305e93198db61a66fbf238db1165b3480e84
Tuan-Tu Tran <tuan-tu.tran@satellit.be>
2020-10-20 16:32:30 +0200

    add RouteRepository implementation

--- InterparkingTest.Application/Services/IRouteRepository.cs
+++ InterparkingTest.Application/Services/IRouteRepository.cs
@@ -1,11 +1,12 @@
 ﻿using System.Collections.Generic;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace InterparkingTest.Application.Commands.CreateRoute
 {
     public interface IRouteRepository
     {
-        Task<List<Route>> GetRoutesAsync();
-        Task SaveRouteAsync(Route route);
+        Task<List<Route>> GetRoutesAsync(CancellationToken cancellationToken = default);
+        Task SaveRouteAsync(Route route, CancellationToken cancellationToken = default);
     }
 }
---
+++ InterparkingTest.Application/Services/RouteRepository.cs
@@ -0,0 +1,33 @@
+﻿using InterparkingTest.Application.Commands.CreateRoute;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace InterparkingTest.Application.Services
+{
+    class RouteRepository : DbContext, IRouteRepository
+    {
+        public DbSet<Route> Routes { get; set; }
+
+        public RouteRepository(DbContextOptions options)
+            : base(options)
+        {
+        }
+
+        public async Task<List<Route>> GetRoutesAsync(CancellationToken cancellationToken = default)
+        {
+            return await Routes.ToListAsync(cancellationToken);
+        }
+
+        public async Task SaveRouteAsync(Route route, CancellationToken cancellationToken = default)
+        {
+            Routes.Attach(route);
+
+            await SaveChangesAsync(cancellationToken);
+        }
+    }
+}
