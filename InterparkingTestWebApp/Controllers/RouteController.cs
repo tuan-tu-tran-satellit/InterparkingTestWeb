59426d8e600e2ca0883edf38dac06ef47aebd653
Tuan-Tu Tran <tuan-tu.tran@satellit.be>
2020-10-20 21:58:13 +0200

    get/save data using IApplicatin

--- InterparkingTestWebApp/Controllers/RouteController.cs
+++ InterparkingTestWebApp/Controllers/RouteController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using InterparkingTest.Application;
 using InterparkingTest.Application.Domain;
@@ -20,9 +21,10 @@ namespace InterparkingTestWebApp.Controllers
             _application = application;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index(CancellationToken cancellationToken)
         {
-            return View(new List<Route>());
+
+            return View(await _application.GetRoutes(cancellationToken));
         }
 
         public IActionResult Create()
@@ -46,11 +48,12 @@ namespace InterparkingTestWebApp.Controllers
         }
 
         [HttpPost]
-        public IActionResult Create(RouteDefinition route)
+        public async Task<IActionResult> Create(RouteDefinition route, CancellationToken cancellationToken)
         {
             if (ModelState.IsValid)
             {
                 _logger.LogDebug("save route: {0}");
+                await _application.AddRouteAsync(route, cancellationToken);
                 return RedirectToAction(nameof(Index));
             }
             return View();
