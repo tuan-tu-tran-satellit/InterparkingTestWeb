e93ffcbd3cb098fe5a5241c337c050fa5d4314a8
Tuan-Tu Tran <tuan-tu.tran@satellit.be>
2020-10-21 08:49:15 +0200

    add some validation data annotations

--- InterparkingTest.Application/Domain/Coordinates.cs
+++ InterparkingTest.Application/Domain/Coordinates.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace InterparkingTest.Application.Domain
 {
     public class Coordinates
     {
+        [Required]
+        [Range(-90,90)]
         public double Latitude { get; set; }
+        [Required]
+        [Range(-180, 180)]
         public double Longitude { get; set; }
     }
 }
--- InterparkingTest.Application/Domain/RouteDefinition.cs
+++ InterparkingTest.Application/Domain/RouteDefinition.cs
@@ -1,17 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace InterparkingTest.Application.Domain
 {
     public class RouteDefinition
     {
+        [Required]
         public Coordinates StartPoint { get; set; }
+        [Required]
         public Coordinates EndPoint { get; set; }
         /// <summary>
         /// In liters/km
         /// </summary>
+        [Range(0, Double.MaxValue, ErrorMessage = "The car consumption must be positive.")]
         public double CarConsumption { get; set; }
+        [Range(0, Double.MaxValue, ErrorMessage = "The engine start effort must be positive.")]
         public double EngineStartEffort { get; set; }
     }
 }
